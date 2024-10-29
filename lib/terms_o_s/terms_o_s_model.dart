e6acd3a5d085b903b45e1553fc016b2e56effeac
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-29 07:52:01 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/terms_o_s/terms_o_s_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'terms_o_s_widget.dart' show TermsOSWidget;
+import 'package:flutter/material.dart';
+
+class TermsOSModel extends FlutterFlowModel<TermsOSWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
---
+++ lib/ticket_confirm/ticket_confirm_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'ticket_confirm_widget.dart' show TicketConfirmWidget;
+import 'package:flutter/material.dart';
+
+class TicketConfirmModel extends FlutterFlowModel<TicketConfirmWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
